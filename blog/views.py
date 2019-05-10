c5fc2d2ea5b6ceb5020dba74aaaf04740cf02a39
sasha-gavriluk <killer.sasha88@gamil.com>
2019-05-11 00:40:14 +0300

    Two Commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import *
 
@@ -19,5 +20,10 @@ class BlogCreateView(CreateView):
 
 class BlogUpdateView(UpdateView):
     model = Post
-    fields = ['title', 'boby']
+    fields = ['title', 'body']
     template_name = "post_edit.html"
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
